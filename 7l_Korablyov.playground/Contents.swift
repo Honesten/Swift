2004536c8596761825e20f3fe4a2fe27a69a97c2
Honesten <honesten@icloud.com>
2021-12-16 00:02:29 +0300

    New file

---
+++ 7l_Korablyov.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+import Foundation
+
+class Machines {
+    private var bots = [
+        "Bob": 356,
+        "Eric": 814,
+        "Carl": 900
+    ]
+
+    func serialNumber(atName name: String) -> Int? {
+        return bots[name]
+    }
+
+    func guardFunc() -> String {
+        guard bots.count < 1 else {
+            return "Боты на месте"
+        }
+        return "Ботов нет"
+    }
+}
+
+let machins = Machines()
+machins.serialNumber(atName: "Bobb")
+machins.guardFunc()
+
+
+
+
